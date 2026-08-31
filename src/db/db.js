e199ba37ac2156f30fc07c3a581f0fc8b4ad0cf9
Karn-yadav06 <ykarn83@gmail.com>
2026-08-31 09:53:53 +0530

    Fix MongoDB connection string in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
 
-    await mongoose.connect("mongodb+srv://ykarn83_db_user:fbgKSQpz6qEWtA6E@bihar.3yijgbb.mongodb.net/day3")
+    await mongoose.connect("/day3")
 
      console.log("Connected to MongoDB successfully");
 }
